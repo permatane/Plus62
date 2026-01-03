7bb72584741407daa658c8213082c9a51a226542
Matane <permatamailbox@gmail.com>
2026-01-03 14:38:49 +0700

    Add files via upload

---
+++ Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
@@ -0,0 +1,8 @@
+package com.rebahin
+
+class Cgvindo : Rebahin() {
+
+    // override var mainUrl = "https://cgvindo2.top/"
+    override var mainUrl = "https://cgvindo2.top/"
+    override var name = "Cgvindo"
+}
