bda64e6ca9e5d6f3d80ed461bd0c89e21ad538e8
Matane <permatamailbox@gmail.com>
2026-01-05 17:00:26 +0700

    Update Auratail.kt

--- Anichin/src/main/kotlin/com/Anichin/Auratail.kt
+++ Anichin/src/main/kotlin/com/Anichin/Auratail.kt
@@ -1,21 +1,20 @@
 package com.Anichin
 
-import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import org.jsoup.Jsoup
 
 class Auratail : Anichin() {
     override var mainUrl              = "https://auratail.vip/"
     override var name                 = "Auratail"
-    override val hasMainPage          = true
-    override var lang                 = "id"
-    override val hasDownloadSupport   = true
-    override val supportedTypes       = setOf(TvType.Movie,TvType.Anime)
+    
 
     override val mainPage = mainPageOf(
         "anime/?status=&type=&order=update" to "Update Terbaru",
     )
+    
+        override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val document = app.get("$mainUrl/${request.data}&page=$page").documentLarge
+        val home     = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
+        return newHomePageResponse(request.name, home)
+    }
 }
-
-
