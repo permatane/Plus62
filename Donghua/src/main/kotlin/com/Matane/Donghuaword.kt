06cb25d126aff7b95bccec0cbe4b160efbd5d9df
Matane <permatamailbox@gmail.com>
2026-01-13 13:43:59 +0700

    Update Donghuaword.kt

--- Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
+++ Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
@@ -16,16 +16,44 @@ class Donghuaword  : Animekhor() {
     override val hasDownloadSupport   = true
     override val supportedTypes       = setOf(TvType.Movie, TvType.Anime)
 
-      override val mainPage = mainPageOf(
-        "anime/?status=ongoing&type=&order=update" to "Recently Updated",
-        "anime/?type=comic&order=update" to "Comic Recently Updated",
-        "anime/?type=comic" to "Comic Series",
-        "anime/?status=&type=ona&sub=&order=update" to "Donghua Recently Updated",
-        "anime/?status=&type=ona" to "Donghua Series",
-        "anime/?status=&sub=&order=latest" to "Latest Added",
-        "anime/?status=&type=&order=popular" to "Popular",
-        "anime/?status=completed&order=update" to "Completed",
-    )
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).documentLarge
+        val title= document.selectFirst("h1.entry-title")?.text()?.trim().toString()
+        val href=document.selectFirst(".eplister li > a")?.attr("href") ?:""
+        var poster = document.select("meta[property=og:image]").attr("content")
+        val description = document.selectFirst("div.entry-content")?.text()?.trim()
+        val type=document.selectFirst(".spe")?.text().toString()
+        val tvtag=if (type.contains("Movie")) TvType.Movie else TvType.TvSeries
+        return if (tvtag == TvType.TvSeries) {
+            val Eppage= document.selectFirst(".eplister li > a")?.attr("href") ?:""
+            val doc= app.get(Eppage).documentLarge
+            val epposter = doc.select("meta[property=og:image]").attr("content")
+            val episodes=doc.select("div.episodelist > ul > li").map { info->
+                        val href1 = info.select("a").attr("href")
+                        val episode = info.select("a span").text().substringAfter("-").substringBeforeLast("-")
+                        newEpisode(href1)
+                        {
+                            this.name=episode
+                            this.posterUrl=epposter
+                        }
+            }
+            newTvSeriesLoadResponse(title, url, TvType.Anime, episodes.reversed()) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        } else {
+            if (poster.isEmpty())
+            {
+                poster=document.selectFirst("meta[property=og:image]")?.attr("content")?.trim().toString()
+            }
+            newMovieLoadResponse(title, url, TvType.Movie, href) {
+                this.posterUrl = poster
+                this.plot = description
+            }
+        }
+    }
+
+    
 
     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
         val document = app.get(data).documentLarge
