d324e207323cf8516bd8c2e29acdddad61e4d823
Matane <permatamailbox@gmail.com>
2026-01-13 15:06:52 +0700

    Update Auratail.kt

--- Donghua/src/main/kotlin/com/Matane/Auratail.kt
+++ Donghua/src/main/kotlin/com/Matane/Auratail.kt
@@ -6,8 +6,8 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.Jsoup
 
 class Auratail : Anichin() {
-    override var mainUrl              = "https://auratail.vip/"
-    override var name                 = "Kazefuri"
+    override var mainUrl              = "https://auratail.vip"
+    override var name                 = "Auratail"
     override val hasMainPage          = true
     override var lang                 = "id"
     override val hasDownloadSupport   = true
