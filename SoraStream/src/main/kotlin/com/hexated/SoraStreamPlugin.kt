db78fe5679bbc1af7dc0c918b9e82403e79c39ad
Matane <permatamailbox@gmail.com>
2026-01-19 12:03:41 +0700

    Update SoraStreamPlugin.kt

--- SoraStream/src/main/kotlin/com/hexated/SoraStreamPlugin.kt
+++ SoraStream/src/main/kotlin/com/hexated/SoraStreamPlugin.kt
@@ -11,7 +11,6 @@ class SoraStreamPlugin: Plugin() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         SoraStream.context = context
         registerMainAPI(SoraStream())
-        registerMainAPI(SoraStreamLite())
         registerExtractorAPI(Jeniusplay2())
     }
 }
