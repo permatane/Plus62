14fef8f82565a8f7ea58897a6a43aa8579b659bb
Matane <permatamailbox@gmail.com>
2026-01-29 13:10:37 +0700

    Update Donghub.kt

--- Donghua/src/main/kotlin/com/Matane/Donghub.kt
+++ Donghua/src/main/kotlin/com/Matane/Donghub.kt
@@ -10,7 +10,7 @@ class Donghub : Anichin() {
         var context: android.content.Context? = null
     }
     override var mainUrl = "https://donghub.vip"
-    override var name = "🐉 Donghub"
+    override var name = "Donghua Donghub"
     override val hasMainPage = true
     override var lang = "id"
     override val hasDownloadSupport = true
