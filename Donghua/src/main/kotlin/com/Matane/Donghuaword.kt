8d3721ecf756b7bb78a1830a43e2cb09173added
Matane <permatamailbox@gmail.com>
2026-01-29 13:10:49 +0700

    Update Donghuaword.kt

--- Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
+++ Donghua/src/main/kotlin/com/Matane/Donghuaword.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.utils.loadExtractor
 
 class Donghuaword  : Animekhor() {
     override var mainUrl              = "https://donghuaworld.com"
-    override var name                 = "🐉 DonghuaWorld"
+    override var name                 = "Donghua DonghuaWorld"
     override val hasMainPage          = true
     override var lang                 = "id"
     override val hasDownloadSupport   = true
