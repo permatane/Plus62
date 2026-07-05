2c87b5b9023e0d40b797092ff64070d61545c0e2
Matane <permatamailbox@gmail.com>
2026-07-05 10:35:23 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,5 @@
-import com.android.build.api.dsl.LibraryExtension
+import com.android.build.gradle.BaseExtension
 import com.lagradost.cloudstream3.gradle.CloudstreamExtension
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.kotlin.dsl.register
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
@@ -13,9 +11,9 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.1")
-        classpath("com.github.recloudstream.gradle:gradle:81b1d424d")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
+        classpath("com.github.recloudstream:gradle:master-SNAPSHOT")  // Perbaikan utama
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.20")
     }
 }
 
@@ -27,41 +25,29 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = 
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(configuration: LibraryExtension.() -> Unit) {
-    extensions.getByName<LibraryExtension>("android").apply {
-        project.extensions.findByType(JavaPluginExtension::class.java)?.apply {
-            // Use Java 17 toolchain even if a higher JDK runs the build.
-            // We still use Java 8 for now which higher JDKs have deprecated.
-            toolchain {
-                languageVersion.set(JavaLanguageVersion.of(17))
-            }
-        }
-
-        configuration()
-    }
-}
+fun Project.android(configuration: BaseExtension.() -> Unit) = 
+    extensions.getByName<BaseExtension>("android").configuration()
 
 subprojects {
     apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/phisher98/cloudstream-extensions-phisher")
-        authors = listOf("Phisher98")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/permatane/Plus62")
+        authors = listOf("Matane")
     }
 
     android {
-        namespace = "com.phisher98"
-        compileSdk = 36
+        namespace = "com.permatane"
 
         defaultConfig {
             minSdk = 21
-        }
-
-        lint {
-            targetSdk = 36
+            compileSdkVersion(35)
+            targetSdk = 35
         }
 
         compileOptions {
@@ -69,42 +55,42 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
-        //noinspection WrongGradleMethod
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
-                    "-Xno-receiver-assertions",
+                    "-Xno-receiver-assertions"
                 )
+                allWarningsAsErrors.set(false)
             }
         }
     }
 
     dependencies {
-        val implementation by configurations
         val cloudstream by configurations
+        val implementation by configurations
+
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // Other dependencies
         implementation(kotlin("stdlib"))
         implementation("com.github.Blatzar:NiceHttp:0.4.18")
-        implementation("org.jsoup:jsoup:1.22.2")
-        implementation("androidx.annotation:annotation:1.10.0")
-        // Do not bump above 2.13.1
+        implementation("org.jsoup:jsoup:1.22.1")
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
         implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-        // Do not bump above 1.8.1
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
         implementation("org.mozilla:rhino:1.8.1")
         implementation("me.xdrop:fuzzywuzzy:1.4.0")
-        implementation("com.google.code.gson:gson:2.14.0")
+        implementation("com.google.code.gson:gson:2.11.0")
         implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
-        implementation("org.bouncycastle:bcpkix-jdk18on:1.84")
+        implementation("app.cash.quickjs:quickjs-android:0.9.2")
+        implementation("com.squareup.okhttp3:okhttp:4.12.0")
+        implementation("androidx.core:core-ktx:1.16.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.11.0")
     }
 }
 
-tasks.register<Delete>("clean") {
+task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
