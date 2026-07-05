271fc35b4ad1b9e5f455c3e94d1b4f54f9f718f8
Matane <permatamailbox@gmail.com>
2026-07-05 12:35:57 +0700

    Update AnimasuPlugin.kt

--- Animasu/src/main/kotlin/com/hexated/AnimasuPlugin.kt
+++ Animasu/src/main/kotlin/com/hexated/AnimasuPlugin.kt
@@ -1,17 +1,18 @@
 package com.hexated
 
-import android.content.Context
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.Plugin
-import com.lagradost.cloudstream3.registerExtractorAPI
-import com.lagradost.cloudstream3.registerMainAPI
+// import com.lagradost.cloudstream3.registerExtractorAPI
+// import com.lagradost.cloudstream3.registerMainAPI
 import com.lagradost.cloudstream3.*
 
 @CloudstreamPlugin
 class AnimasuPlugin: Plugin() {
-    override fun load(context: Context) {
+    override fun load() {
         // All providers should be added in this manner. Please don't edit the providers list directly.
-        Animasu.context = context
+       // Animasu.context = context
         registerMainAPI(Animasu())
         registerExtractorAPI(Archivd())
         registerExtractorAPI(Newuservideo())
