1bc163790e2b226ba05e289ea406b731e2258488
Matane <permatamailbox@gmail.com>
2026-07-05 11:32:10 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,17 @@ subprojects {
             targetCompatibility = JavaVersion.VERSION_1_8
         }
 
+        // Nonaktifkan minification untuk menghindari R8 error
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            }
+            debug {
+                isMinifyEnabled = false
+            }
+        }
+
         tasks.withType<KotlinJvmCompile> {
             compilerOptions {
                 jvmTarget.set(JvmTarget.JVM_1_8)
