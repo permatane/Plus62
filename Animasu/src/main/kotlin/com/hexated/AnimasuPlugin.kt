a5409c365789e9e0cf6220f4829617545e2af32f
Matane <permatamailbox@gmail.com>
2026-07-05 12:29:14 +0700

    Update AnimasuPlugin.kt

--- Animasu/src/main/kotlin/com/hexated/AnimasuPlugin.kt
+++ Animasu/src/main/kotlin/com/hexated/AnimasuPlugin.kt
@@ -5,6 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import com.lagradost.cloudstream3.registerExtractorAPI
 import com.lagradost.cloudstream3.registerMainAPI
+import com.lagradost.cloudstream3.*
 
 @CloudstreamPlugin
 class AnimasuPlugin: Plugin() {
