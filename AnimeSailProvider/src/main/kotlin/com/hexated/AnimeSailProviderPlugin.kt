f613f0d331151c3760a2f09368eaf3882f3d7d52
Matane <permatamailbox@gmail.com>
2026-07-05 12:19:51 +0700

    Update AnimeSailProviderPlugin.kt

--- AnimeSailProvider/src/main/kotlin/com/hexated/AnimeSailProviderPlugin.kt
+++ AnimeSailProvider/src/main/kotlin/com/hexated/AnimeSailProviderPlugin.kt
@@ -1,9 +1,9 @@
-
 package com.hexated
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
+import com.lagradost.cloudstream3.registerMainAPI
 
 @CloudstreamPlugin
 class AnimeSailProviderPlugin: Plugin() {
