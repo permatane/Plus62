55958f57ceeac0fc2c9eb946c8b88e311107c436
Matane <permatamailbox@gmail.com>
2026-07-24 17:23:49 +0700

    Delete Rebahin directory

--- Rebahin/src/main/kotlin/com/rebahin/Cgvindo.kt
+++
@@ -1,8 +0,0 @@
-package com.rebahin
-
-class Cgvindo : Rebahin() {
-
-    // override var mainUrl = "https://cgvindo2.top/"
-    override var mainUrl = "https://cgvindo2.top/"
-    override var name = "Cgvindo"
-}
