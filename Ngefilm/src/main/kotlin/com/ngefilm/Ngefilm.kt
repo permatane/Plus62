f4acf8c896d5033620363ae7f0cc0ed8e2a667f2
Matane <permatamailbox@gmail.com>
2026-07-25 10:49:56 +0700

    Update Ngefilm.kt

--- Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
@@ -17,7 +17,7 @@ import org.jsoup.nodes.Element
 
 class Ngefilm : MainAPI() {
 
-    override var mainUrl = "https://ngefilm.live"
+    override var mainUrl = "https://new39.ngefilm.site"
     private var directUrl: String? = null
     override var name = "Ngefilm21"
     override val hasMainPage = true
