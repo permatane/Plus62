74d14ab267bc6259709d9a4bc36ccb628f6f7631
Matane <permatamailbox@gmail.com>
2026-07-28 12:08:32 +0700

    Update Ngefilm.kt

--- Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
@@ -196,4 +196,74 @@ private fun Element.toSearchResult(): SearchResponse? {
             }
         }
     }
+
+    override suspend fun loadLinks(
+            data: String,
+            isCasting: Boolean,
+            subtitleCallback: (SubtitleFile) -> Unit,
+            callback: (ExtractorLink) -> Unit
+    ): Boolean {
+
+        val document = app.get(data).document
+        val id = document.selectFirst("div#muvipro_player_content_id")?.attr("data-id")
+
+        if (id.isNullOrEmpty()) {
+            document.select("ul.muvipro-player-tabs li a").amap { ele ->
+                val iframe =
+                        app.get(fixUrl(ele.attr("href")))
+                                .document
+                                .selectFirst("div.gmr-embed-responsive iframe")
+                                .getIframeAttr()
+                                ?.let { httpsify(it) }
+                                ?: return@amap
+
+                loadExtractor(iframe, "$directUrl/", subtitleCallback, callback)
+            }
+        } else {
+            document.select("div.tab-content-ajax").amap { ele ->
+                val server =
+                        app.post(
+                                        "$directUrl/wp-admin/admin-ajax.php",
+                                        data =
+                                                mapOf(
+                                                        "action" to "muvipro_player_content",
+                                                        "tab" to ele.attr("id"),
+                                                        "post_id" to "$id"
+                                                )
+                                )
+                                .document
+                                .select("iframe")
+                                .attr("src")
+                                .let { httpsify(it) }
+
+                loadExtractor(server, "$directUrl/", subtitleCallback, callback)
+            }
+        }
+
+        return true
+    }
+
+    private fun Element.getImageAttr(): String {
+        return when {
+            this.hasAttr("data-src") -> this.attr("abs:data-src")
+            this.hasAttr("data-lazy-src") -> this.attr("abs:data-lazy-src")
+            this.hasAttr("srcset") -> this.attr("abs:srcset").substringBefore(" ")
+            else -> this.attr("abs:src")
+        }
+    }
+
+    private fun Element?.getIframeAttr(): String? {
+        return this?.attr("data-litespeed-src").takeIf { it?.isNotEmpty() == true }
+                ?: this?.attr("src")
+    }
+
+    private fun String?.fixImageQuality(): String? {
+        if (this == null) return null
+        val regex = Regex("(-\\d*x\\d*)").find(this)?.groupValues?.get(0) ?: return this
+        return this.replace(regex, "")
+    }
+
+    private fun getBaseUrl(url: String): String {
+        return URI(url).let { "${it.scheme}://${it.host}" }
+    }
 }
