4218285e5baccabfe8ffe6ed48c769738390f562
Matane <permatamailbox@gmail.com>
2026-07-28 15:33:21 +0700

    Update Donghuaid.kt

--- Donghua/src/main/kotlin/com/Matane/Donghuaid.kt
+++ Donghua/src/main/kotlin/com/Matane/Donghuaid.kt
@@ -21,7 +21,17 @@ class Donghuaid : Anichin() {
         "/anime/?status=completed&type=&order=" to "Complete",
 
     )
-
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val url = buildPagedUrl(request.data, page)
+        val document = app.get(url, headers = siteHeaders, referer = mainUrl).document
+        val results = parseDonghuaCards(document, includeSidebar = page == 1 && request.data.removeSuffix("/") == mainUrl.removeSuffix("/"))
+            .distinctBy { it.url.normalizedKey() }
+        val hasNext = document.selectFirst(
+            "a.next[href], a.next.page-numbers[href], link[rel=next], .hpage a[href*='page=${page + 1}'], a[href*='/page/${page + 1}/'], a[href*='page=${page + 1}']"
+        ) != null
+        return newHomePageResponse(request.name, results, hasNext)
+    }
+    
    override suspend fun loadLinks(
             data: String,
             isCasting: Boolean,
