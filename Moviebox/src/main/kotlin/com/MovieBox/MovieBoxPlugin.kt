b608a5bde1c892fb4b853c5a41bf49f54d41e0b5
Matane <permatamailbox@gmail.com>
2026-07-28 12:58:45 +0700

    Update MovieBoxPlugin.kt

--- Moviebox/src/main/kotlin/com/MovieBox/MovieBoxPlugin.kt
+++ Moviebox/src/main/kotlin/com/MovieBox/MovieBoxPlugin.kt
@@ -1,13 +1,12 @@
 package com.MovieBox
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class MovieBoxPlugin: Plugin() {
-    override fun load(context: Context) {
-        // All providers should be added in this manner. Please don't edit the providers list directly.
+class MvieBoxPlugin: BasePlugin() {
+    override fun load() {
         registerMainAPI(MovieBox())
+        
     }
 }
