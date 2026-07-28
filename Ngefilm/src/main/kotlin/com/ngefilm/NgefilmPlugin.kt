26ab5f7750db832d06ea73c0ce6710bbed54dc3a
Matane <permatamailbox@gmail.com>
2026-07-28 11:48:32 +0700

    Update NgefilmPlugin.kt

--- Ngefilm/src/main/kotlin/com/ngefilm/NgefilmPlugin.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/NgefilmPlugin.kt
@@ -1,7 +1,7 @@
 package com.ngefilm
 
 import android.content.Context
-import com.lagradost.cloudstream3.extractors.*
+import com.lagradost.cloudstream3.extractors.Chillx
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
