884f5d1167044e9ce9fa1bed7cfa92aab4e8868c
Matane <permatamailbox@gmail.com>
2026-07-28 11:38:30 +0700

    Update Ngefilm.kt

--- Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
+++ Ngefilm/src/main/kotlin/com/ngefilm/Ngefilm.kt
@@ -7,23 +7,12 @@ import com.lagradost.cloudstream3.mainPageOf
 class Ngefilm : MainAPI() {
 
     override var mainUrl = "https://new39.ngefilm.site"
-    private var directUrl: String? = null
+  //  private var directUrl: String? = null
     override var name = "Ngefilm21"
     override val hasMainPage = true
     override var lang = "id"
     override val supportedTypes =
             setOf(TvType.Movie, TvType.TvSeries, TvType.Anime, TvType.AsianDrama)
-
-    private suspend fun updateToLatestDomain() {
-        if (mainUrl.contains("ngefilm.live")) {
-            val doc = app.get(mainUrl).document
-            // Cari link domain baru 
-            val newLink = doc.selectFirst("a[href*='ngefilm'], strong a, p a[href^='https://']")?.attr("href")
-            if (!newLink.isNullOrBlank() && newLink.contains("ngefilm")) {
-                mainUrl = newLink.substringBeforeLast("/", "").substringBefore("?")
-            }
-        }
-    }
 
     override val mainPage =
             mainPageOf(
@@ -41,10 +30,4 @@ class Ngefilm : MainAPI() {
                     "country/china/page/%d/" to "Film China",
 
             )
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse { updateToLatestDomain()
-    val data = request.data.format(page)
-    val document = app.get("$mainUrl/$data").document
-//    val home = document.select("article.item").mapNotNull { it.toSearchResult() }
-//   return newHomePageResponse(request.name, home)
-    }
 }
