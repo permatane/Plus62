14abef03b0fae9fd4362582a811292b48a4ed4cc
Matane <permatamailbox@gmail.com>
2026-07-29 10:16:43 +0700

    Create Anixverse.kt

---
+++ Donghua/src/main/kotlin/com/Matane/Anixverse.kt
@@ -0,0 +1,53 @@
+package com.Matane
+
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import org.jsoup.Jsoup
+
+class Anixverse : Anichin() {
+    override var mainUrl              = "https://anixcafe.com"
+    override var name                 = "Donghua Anixverse"
+    override val hasMainPage          = true
+    override var lang                 = "id"
+    override val hasDownloadSupport   = true
+    override val supportedTypes       = setOf(TvType.Movie,TvType.Anime)
+
+
+    override val mainPage = mainPageOf(
+        "anime/?status=&type=&order=update" to "Update Terbaru",
+        "anime/?status=&type=&order=popular" to "Paling Populer",
+        "anime/?" to "Donghua",
+        "/anime/?status=&type=movie&sub=&order=" to "Movies",
+        "/anime/?status=completed&type=&sub=&order=" to "Complete",
+        "/anime/?status=upcoming&sub=" to "Upcoming",
+
+    )
+
+
+   override suspend fun loadLinks(
+            data: String,
+            isCasting: Boolean,
+            subtitleCallback: (SubtitleFile) -> Unit,
+            callback: (ExtractorLink) -> Unit
+    ): Boolean {
+
+        val document = app.get(data).document
+        document.select("div.mobius > select.mirror > option")
+                .mapNotNull {
+                    fixUrl(Jsoup.parse(base64Decode(it.attr("value"))).select("iframe").attr("src"))
+                }
+                .amap {
+                    if (it.startsWith(mainUrl)) {
+                        app.get(it, referer = "$mainUrl/").document.select("iframe").attr("src")
+                    } else {
+                        it
+                    }
+                }
+                .amap { loadExtractor(httpsify(it), data, subtitleCallback, callback) }
+
+        return true
+    }
+}
+
+
