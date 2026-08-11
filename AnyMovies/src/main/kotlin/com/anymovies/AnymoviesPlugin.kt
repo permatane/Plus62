09d0801d014d29494ec286ec4d166fc62c74c28d
Matane <permatamailbox@gmail.com>
2026-08-11 11:12:41 +0700

    Update AnymoviesPlugin.kt

--- AnyMovies/src/main/kotlin/com/anymovies/AnymoviesPlugin.kt
+++ AnyMovies/src/main/kotlin/com/anymovies/AnymoviesPlugin.kt
@@ -3,10 +3,23 @@ package com.anymovies
 import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import com.lagradost.cloudstream3.extractors.*
 
 @CloudstreamPlugin
 class AnymoviesPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Anymovies())
+        registerExtractorAPI(D0000d())
+        registerExtractorAPI(Cdnplayer())
+        registerExtractorAPI(CdnwishCom())
+        registerExtractorAPI(Gdriveplayer())  
+        registerExtractorAPI(XStreamCdn())
+        registerExtractorAPI(Vidtren())    
+        registerExtractorAPI(svilla())
+        registerExtractorAPI(svanila())
+        registerExtractorAPI(Vidguardto())
+        registerExtractorAPI(Vidguardto1())
+        registerExtractorAPI(Vidguardto2())
+        registerExtractorAPI(Vidguardto3()) 
     }
 }
