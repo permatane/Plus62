4f96c5abe9a04982983a06b72b87253b2781487c
Matane <permatamailbox@gmail.com>
2026-08-11 14:20:13 +0700

    Update Yunshanid.kt

--- Donghua/src/main/kotlin/com/Matane/Yunshanid.kt
+++ Donghua/src/main/kotlin/com/Matane/Yunshanid.kt
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
-class Yunshanid : MainAPI() {
+class Yunshanid : : Anichin() {
 
     // ==========================================
     // PROVIDER CONFIG
